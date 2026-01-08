88d4d53e15a64668496c563612d6cd68cec3ba38
ShayThorn97 <shaythorn@hotmail.co.uk>
2026-01-08 01:40:26 +0000

    cleaned up the file a bit

--- bnc.h
+++ bnc.h
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// TODO
+// - could possibly add some enums for configuration, OS, directories etc
+
 #define BNC_CMD_CAP 4096
 
 typedef struct {
@@ -11,38 +14,34 @@ typedef struct {
     size_t cmdLen;
 } Executable;
 
-#define bnc_AddCommand(exe, ...) bnc_CmdJoin(exe, __VA_ARGS__, NULL)
+#define bnc_Add(exe, ...) bnc_Join(exe, __VA_ARGS__, NULL)
 
-static inline void bnc_CmdAppend(Executable* exe, const char* file) {
-    if (exe->cmdLen > 0) {
-        memcpy(exe->cmd + exe->cmdLen, " ", 1);
-        exe->cmdLen++;
-    }
-
-    memcpy(exe->cmd + exe->cmdLen, file, strlen(file));
-    exe->cmdLen += strlen(file);
-}
-
-static inline void bnc_CmdJoin(Executable* exe, const char* first, ...) {
+static inline void bnc_Join(Executable* exe, const char* first, ...) {
     va_list args;
     va_start(args, first);
 
     const char* s = first;
     while (s) {
-        bnc_CmdAppend(exe, s);
-        s = va_arg(args, const char*);
+        if (exe->cmdLen > 0) {
+            memcpy(exe->cmd + exe->cmdLen, " ", 1);
+            exe->cmdLen++;
+
+            memcpy(exe->cmd + exe->cmdLen, s, strlen(s));
+            exe->cmdLen += strlen(s);
+            s = va_arg(args, const char*);
+        }
     }
 
     va_end(args);
 }
 
-static inline void bnc_CmdPrint(Executable* exe) {
+static inline void bnc_Print(Executable* exe) {
     // we need to add an assert
     exe->cmd[exe->cmdLen] = '\0';
     printf("%s", exe->cmd);
 }
 
-static inline void bnc_CmdBuild(Executable* exe) {
+static inline void bnc_Build(Executable* exe) {
     // we need to assert here
     exe->cmd[exe->cmdLen] = '\0';
     system(exe->cmd);
