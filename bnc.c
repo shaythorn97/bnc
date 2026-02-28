b935b3bb753fb307b18809e39c04bc646c89a63a
ShayThorn97 <shaythorn@hotmail.co.uk>
2026-02-28 13:59:00 +0000

    made the API better

--- bnc.c
+++ bnc.c
@@ -1,39 +1,48 @@
-#include "bnkbuild.h"
+#define BNC_IMPLEMENTATION
+#include "bnc.h"
 
 int main() {
     Executable exe = {0};
 
-    bnc_AddCommand(&exe, "cl");
-    bnc_AddCommand(&exe, 
-            "src/main.c",
-            "src/renderer.c",
-            "src/camera.c",
-            "src/bnkgl.c",
-            "src/eventmanager.c",
-            "src/shader.c",
+    add_source_files(&exe, "src/main.c", "src/array.c");
+    add_include_dirs(&exe, "external/include", "external/maths/include");
+    add_library_files(&exe, "user32.lib", "external/lib/mylib.lib");
 
-            "external/glad/src/glad.c");
-    bnc_AddCommand(&exe, 
-            "/I external/glfw/include",
-            "/I external/glad/include");
-    bnc_AddCommand(&exe, 
-            "/Fe: build/main.exe",
-            "/Fo: build/",
-            "/Fd: build/");
-    bnc_AddCommand(&exe, 
-            "/Zi",
-            "/MD",
-            "/W1",
-            "/fsanitize=address");
-    bnc_AddCommand(&exe, 
-            "external/glfw/lib/glfw3.lib",
+    print_target(&exe);
 
-            "opengl32.lib",
-            "user32.lib",
-            "gdi32.lib",
-            "shell32.lib",
-            "vcruntime.lib",
-            "msvcrt.lib");
+    //build_target(&exe);
 
-    bnc_CmdPrint(&exe);
+    // bnc_AddCommand(&exe, "cl");
+    // bnc_AddCommand(&exe, 
+    //         "src/main.c",
+    //         "src/renderer.c",
+    //         "src/camera.c",
+    //         "src/bnkgl.c",
+    //         "src/eventmanager.c",
+    //         "src/shader.c",
+
+    //         "external/glad/src/glad.c");
+    // bnc_AddCommand(&exe, 
+    //         "/I external/glfw/include",
+    //         "/I external/glad/include");
+    // bnc_AddCommand(&exe, 
+    //         "/Fe: build/main.exe",
+    //         "/Fo: build/",
+    //         "/Fd: build/");
+    // bnc_AddCommand(&exe, 
+    //         "/Zi",
+    //         "/MD",
+    //         "/W1",
+    //         "/fsanitize=address");
+    // bnc_AddCommand(&exe, 
+    //         "external/glfw/lib/glfw3.lib",
+
+    //         "opengl32.lib",
+    //         "user32.lib",
+    //         "gdi32.lib",
+    //         "shell32.lib",
+    //         "vcruntime.lib",
+    //         "msvcrt.lib");
+
+    // bnc_CmdPrint(&exe);
 }
--- bnc.h
+++ bnc.h
@@ -5,44 +5,143 @@
 #include <stdio.h>
 
 // TODO
-// - could possibly add some enums for configuration, OS, directories etc
+// - for now lets strip the namespace
+// - we could add shadowing data to the backend, like stb headers
 
-#define BNC_CMD_CAP 4096
+#define BNC_BUF_SIZE 1024 * 8
+
+typedef enum {
+    BNC_MSVC,
+} CompilerType;
+
+typedef enum {
+    BNC_DEBUG,
+    BNC_RELEASE
+} ConfigType;
 
 typedef struct {
-    char cmd[BNC_CMD_CAP];
+    char cmd[BNC_BUF_SIZE];
     size_t cmdLen;
+    CompilerType compiler;
+    ConfigType config;
+    const char* name;
+    const char* outDir;
 } Executable;
 
-#define bnc_Add(exe, ...) bnc_Join(exe, __VA_ARGS__, NULL)
+#define add_source_files(exe, ...) bnc_join_default(exe, __VA_ARGS__, (char*)NULL)
+#define add_include_dirs(exe, ...) bnc_join_include(exe, __VA_ARGS__, (char*)NULL)
+#define add_library_files(exe, ...) bnc_join_default(exe, __VA_ARGS__, (char*)NULL)
+#define add_options(exe, ...) bnc_join_default(exe, __VA_ARGS__, (char*)NULL)
 
-static inline void bnc_Join(Executable* exe, const char* first, ...) {
+void print_target(Executable* exe);
+void build_target(Executable* exe);
+
+void bnc_join_default(Executable* exe, const char* first, ...);
+void bnc_join_include(Executable* exe, const char* first, ...);
+
+#ifdef BNC_IMPLEMENTATION
+
+void build_command(Executable* exe) {
+    char temp[BNC_BUF_SIZE]; 
+    int tempLen = exe->cmdLen;
+    memcpy(temp, exe->cmd, exe->cmdLen);
+    exe->cmdLen = 0;
+
+    const char* compiler = "";
+    const char* config = "";
+    
+    if (!exe->name) {
+        exe->name = "a";
+    }
+
+    switch (exe->compiler) {
+    case BNC_MSVC: 
+        compiler = "cl ";
+        break;
+    }
+
+    switch (exe->config) {
+    case BNC_DEBUG:
+        config = "/Zi ";
+        break;
+    }
+    
+    sprintf(exe->cmd, "%s%s", compiler, config);
+    exe->cmdLen += strlen(compiler) + strlen(config);
+
+    const char* fo = "/Fo:";
+    const char* fe = "/Fe:";
+    const char* fd = "/Fd:";
+
+    const char* sep = "/";
+    if (!exe->outDir) {
+        sep = "";
+        exe->outDir = "";
+    } else {
+        const char* suffix = "/ ";
+        sprintf(exe->cmd + exe->cmdLen, "%s%s%s", fo, exe->outDir, suffix);
+        exe->cmdLen += strlen(fo) + strlen(exe->outDir) + strlen(suffix);
+    }
+
+    sprintf(exe->cmd + exe->cmdLen, "%s%s%s%s ", fe, exe->outDir, sep, exe->name);
+    exe->cmdLen += strlen(fe) + strlen(exe->outDir) + strlen(sep) + strlen(exe->name) + 1;
+
+    sprintf(exe->cmd + exe->cmdLen, "%s%s%s%s ", fd, exe->outDir, sep, exe->name);
+    exe->cmdLen += strlen(fd) + strlen(exe->outDir) + strlen(sep) + strlen(exe->name) + 1;
+
+    memcpy(exe->cmd + exe->cmdLen, temp, tempLen);
+    exe->cmdLen += tempLen;
+}
+
+void print_target(Executable* exe) {
+    build_command(exe);
+    exe->cmd[exe->cmdLen] = '\0';
+    printf("%s", exe->cmd);
+}
+
+void build_target(Executable* exe) {
+    build_command(exe);
+    exe->cmd[exe->cmdLen] = '\0';
+    system(exe->cmd);
+}
+
+void bnc_join_default(Executable* exe, const char* first, ...) {
     va_list args;
     va_start(args, first);
 
     const char* s = first;
     while (s) {
         if (exe->cmdLen > 0) {
-            memcpy(exe->cmd + exe->cmdLen, " ", 1);
-            exe->cmdLen++;
-
-            memcpy(exe->cmd + exe->cmdLen, s, strlen(s));
-            exe->cmdLen += strlen(s);
-            s = va_arg(args, const char*);
+            exe->cmd[exe->cmdLen++] = ' ';
         }
+
+        memcpy(exe->cmd + exe->cmdLen, s, strlen(s));
+        exe->cmdLen += strlen(s);
+        s = va_arg(args, const char*);
     }
 
     va_end(args);
 }
 
-static inline void bnc_Print(Executable* exe) {
-    // we need to add an assert
-    exe->cmd[exe->cmdLen] = '\0';
-    printf("%s", exe->cmd);
+void bnc_join_include(Executable* exe, const char* first, ...) {
+    va_list args;
+    va_start(args, first);
+
+    const char* prefix = " /I ";
+
+    const char* s = first;
+    while (s) {
+        if (exe->cmdLen > 0) {
+            memcpy(exe->cmd + exe->cmdLen, prefix, strlen(prefix));
+            exe->cmdLen += strlen(prefix);
+        }
+
+        memcpy(exe->cmd + exe->cmdLen, s, strlen(s));
+        exe->cmdLen += strlen(s);
+        s = va_arg(args, const char*);
+    }
+
+    va_end(args);
 }
 
-static inline void bnc_Build(Executable* exe) {
-    // we need to assert here
-    exe->cmd[exe->cmdLen] = '\0';
-    system(exe->cmd);
-}
+#endif
